cf392f3d2e3c611810534aae0444dfa0393aab41
François-Xavier Mousseau <fxmousseau@hotmail.com>
2018-03-06 15:49:04 -0500

    Ajout de service à la page de modification de projet

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -5,15 +5,9 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { AddCategorieComponent } from '../add-categorie/add-categorie.component';
 import { AddCorpusComponent } from '../add-corpus/add-corpus.component';
 import { AddAnnotateurComponent } from '../add-annotateur/add-annotateur.component';
-import {
-  AngularFirestore,
-  AngularFirestoreCollection,
-  AngularFirestoreDocument
-} from 'angularfire2/firestore';
-import * as firebase from 'firebase';
+import { ProjectService } from './project.service';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/Rx';
-import 'rxjs/add/operator/mergeMap';
+
 
 @Component({
   selector: 'app-project',
@@ -24,15 +18,14 @@ import 'rxjs/add/operator/mergeMap';
 export class ProjectComponent implements OnInit, OnDestroy {
   currentProject: Project;
   private sub: any;
-  categoryExist: boolean;
   isDataLoaded: boolean;
-  ajout: boolean;
 
   annotateurs: Observable<any[]>;
   categories: Observable<any[]>;
   corpus: Observable<any[]>;
 
-  constructor(private activeRouter: ActivatedRoute, public dialog: MatDialog, private afs: AngularFirestore, private router: Router) { }
+  constructor(private activeRouter: ActivatedRoute, public dialog: MatDialog, private ps: ProjectService,
+    private router: Router) { }
 
   ngOnInit() {
     this.sub = this.activeRouter.params.subscribe(params => {
@@ -41,9 +34,9 @@ export class ProjectComponent implements OnInit, OnDestroy {
 
     });
 
-    this.annotateurs = this.getAnnotateur(this.currentProject.id);
-    this.categories = this.getCategories(this.currentProject.id);
-    this.corpus = this.getCorpus(this.currentProject.id);
+    this.annotateurs = this.ps.getAnnotateur(this.currentProject.id);
+    this.categories = this.ps.getCategories(this.currentProject.id);
+    this.corpus = this.ps.getCorpus(this.currentProject.id);
 
     this.isDataLoaded = true;
   }
@@ -52,64 +45,39 @@ export class ProjectComponent implements OnInit, OnDestroy {
     this.sub.unsubscribe;
   }
 
-  getCorpus(projectId: string): Observable<any[]> {
-    return this.afs.collection('Corpus', ref => ref.where('projectId', '==', projectId)).valueChanges();
-  }
-
-  getCategories(projectId: string): Observable<any[]> {
-
-    return this.afs.collection('Categories', ref => ref.where('projectId', '==', projectId)).valueChanges();
-
-  }
-
-  // Trouve tous les annotateurs du projet
-  getAnnotateur(projectId: string): Observable<any[]> {
-
-    const annotateursCollection = this.afs.collection('Annotateurs', ref => ref.where('projectId', '==', projectId));
-
-    const users = annotateursCollection.snapshotChanges().map(changes => {
-      return changes.map(a => {
-        const data = a.payload.doc.data();
-
-        const userId = data.userId;
-
-        return this.afs.collection('Users').doc(userId).snapshotChanges().take(1).map(actions => {
-          return actions.payload.data();
-        }).map(signup => {
-          return { id: signup.id, firstName: signup.firstName, lastName: signup.lastName }
-        });
-
-      });
-    }).flatMap(user => Observable.combineLatest(user));
-
-    return users;
-  }
-
   // Sauvegarde les modification apporté au projet
   saveProjectModification() {
-    /*if (this.currentProject.title != null && this.currentProject.title !== '' && this.currentProject.description != null &&
+    if (this.currentProject.title != null && this.currentProject.title !== '' && this.currentProject.description != null &&
       this.currentProject.description !== '') {
 
-      this.afs.collection('Projects').doc(this.currentProject.id).set(this.currentProject);
+      this.ps.saveProject(this.currentProject);
 
       alert('Modification Sauvegarder');
-      this.router.navigate(['/']);
-    }*/
+      // this.router.navigate(['/']);
+    }
 
   }
 
+  // Événement lorsqu'un texte est sélectionné
   corpusSelected(corpus: any) {
     console.log(corpus);
   }
 
-  deleteCategories(categorie: any) {
-    console.log(categorie);
-    this.afs.collection('Categories').doc(categorie.id).delete();
+  // Supprime un texte
+  deleteCorpus(corpus: any) {
+    console.log(corpus);
+    // pas implémenté pour le moment
+    // this.ps.deleteCorpus(corpus.id);
   }
 
+  // Supprime une catégorie
+  deleteCategories(categorie: any) {
+    this.ps.deleteCategories(categorie.id);
+  }
+
+  // Supprime un annotateur
   deleteAnnotateur(user: any) {
-    console.log(user);
-    // delete an annotateur
+    this.ps.deleteAnnotateur(user.id, this.currentProject.id);
   }
 
 
@@ -121,16 +89,12 @@ export class ProjectComponent implements OnInit, OnDestroy {
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      if (result.corpusTitle !== undefined && result.corpusFile !== undefined) {
+      if (result !== undefined) {
+        if (result.corpusTitle !== undefined && result.corpusFile !== undefined) {
 
-        // est-ce qu'il y a une validation ici a faire? (titre du fichier déjà existant?)
-
-        const corpusId = this.afs.createId();
-
-        this.afs.collection('Corpus').doc(corpusId)
-          .set({ 'id': corpusId, 'projectId': this.currentProject.id, 'title': result.corpusTitle });
-
-        firebase.storage().ref().child('Projets/' + corpusId + '/' + result.corpusTitle).put(result.corpusFile);
+          // est-ce qu'il y a une validation ici a faire? (titre du fichier déjà existant?)
+          this.ps.addCorpus(result, this.currentProject.id);
+        }
       }
     });
 
@@ -147,39 +111,8 @@ export class ProjectComponent implements OnInit, OnDestroy {
 
       if (result !== undefined) {
         if (result.categoryName !== undefined && result.categoryColor !== undefined) {
-          this.categoryExist = false;
-          this.ajout = true;
 
-          // véfication si la couleur ou le nom de la catégorie est déjà utilisé.
-          this.afs.collection('Categories', ref => ref.where('projectId', '==', this.currentProject.id)).valueChanges()
-            .subscribe(items => {
-              console.log('before' + this.ajout);
-              if (this.ajout === true) {
-                items.forEach((item: any) => {
-
-                  if (item.color === result.categoryColor) {
-                    this.categoryExist = true;
-                    console.log('La couleur choisit est déjà utilisé par une autre catégorie');
-                  } else if ((item.name as string).toUpperCase() === (result.categoryName as string).toUpperCase()) {
-                    this.categoryExist = true;
-                    console.log('Le nom choisit est déjà utilisé par une autre catégorie');
-                  }
-                  console.log(this.categoryExist);
-                });
-
-                console.log('before add' + this.ajout);
-                if (this.categoryExist === false) {
-                  console.log('ajouté');
-                  const categorieId = this.afs.createId();
-                  // ajoute la nouvelle catégorie
-                  this.afs.collection('Categories').doc(categorieId)
-                  .set({ 'id': categorieId, 'projectId': this.currentProject.id,
-                  'color': result.categoryColor, 'name': result.categoryName });
-                  this.ajout = false;
-                }
-                console.log('after add' + this.ajout);
-              }
-            });
+          this.ps.addCategorie(result, this.currentProject.id);
         }
       }
     });
@@ -195,12 +128,7 @@ export class ProjectComponent implements OnInit, OnDestroy {
     dialogRef.afterClosed().subscribe(result => {
       if (result !== undefined) {
 
-        // manque la validation pour que le même utilisateur ne soit pas mis dans la liste deux fois
-        const annotateurId = this.afs.createId();
-
-        this.afs.collection('Annotateurs').doc(annotateurId)
-        .set({'id': annotateurId, 'Userid': result.id, 'projectId': this.currentProject.id});
-
+        this.ps.addAnnotateur(result, this.currentProject.id);
       }
     });
 
--- src/app/components/project/project.service.ts
+++ src/app/components/project/project.service.ts
@@ -6,6 +6,7 @@ import {
 } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase';
+import { Project } from '../../shared/project.class';
 
 
 @Injectable()
@@ -16,8 +17,139 @@ export class ProjectService {
 
   }
 
-  getProject(id: string): any {
+  // Trouve les textes du projet sélectionné
+  getCorpus(projectId: string): Observable<any[]> {
+    return this.afs.collection('Corpus', ref => ref.where('projectId', '==', projectId)).valueChanges();
+  }
 
+  // Trouve les catégories du projet sélectionné
+  getCategories(projectId: string): Observable<any[]> {
+
+    return this.afs.collection('Categories', ref => ref.where('projectId', '==', projectId)).valueChanges();
+
+  }
+
+  // Trouve tous les annotateurs du projet
+  getAnnotateur(projectId: string): Observable<any[]> {
+
+    const annotateursCollection = this.afs.collection('Annotateurs', ref => ref.where('projectId', '==', projectId));
+
+    const users = annotateursCollection.snapshotChanges().map(changes => {
+      return changes.map(a => {
+        const data = a.payload.doc.data();
+
+        const userId = data.userId;
+
+        return this.afs.collection('Users').doc(userId).snapshotChanges().take(1).map(actions => {
+          return actions.payload.data();
+        }).map(signup => {
+          return { id: signup.id, firstName: signup.firstName, lastName: signup.lastName }
+        });
+
+      });
+    }).flatMap(user => Observable.combineLatest(user));
+
+    return users;
+  }
+
+  saveProject(project: Project) {
+    this.afs.collection('Projects').doc(project.id)
+      .update({ 'description': project.description, 'title': project.title });
+  }
+
+  // Supprime un texte
+  deleteCorpus(corpusId: string) {
+    console.log(corpusId);
+    // pas implémenté pour le moment
+  }
+
+  // Supprime une catégorie
+  deleteCategories(categorieId: string) {
+    this.afs.collection('Categories').doc(categorieId).delete();
+  }
+
+  // Supprime un annotateur
+  deleteAnnotateur(userId: string, projectId: string) {
+    this.afs.collection('Annotateurs').ref.where('userId', '==', userId)
+      .where('projectId', '==', projectId).get().then(querySnapshot => {
+        const batch = this.afs.firestore.batch();
+
+        querySnapshot.forEach(doc => {
+
+          batch.delete(doc.ref);
+        });
+
+        return batch.commit();
+      }).then(() => console.log('Annotateur supprimmer'));
+  }
+
+  // Ajoute un nouveau texte
+  addCorpus(corpus: any, projectId: string) {
+    const corpusId = this.afs.createId();
+
+    this.afs.collection('Corpus').doc(corpusId)
+      .set({ 'id': corpusId, 'projectId': projectId, 'title': corpus.corpusTitle });
+
+    firebase.storage().ref().child('Projets/' + corpusId + '/' + corpus.corpusTitle).put(corpus.corpusFile);
+  }
+
+  // Ajoute un nouvel annotateur
+  addAnnotateur(annotateur: any, projectId: string) {
+    let annotateurExist = false;
+
+    this.afs.collection('Annotateurs').ref.where('projectId', '==', projectId).get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+
+        if (doc.data().userId === annotateur.id) {
+          annotateurExist = true;
+          console.log('Utilisateur est déjà un annotateur pour ce projet');
+        }
+
+      });
+
+      if (annotateurExist === false) {
+        const annotateurId = this.afs.createId();
+
+        this.afs.collection('Annotateurs').doc(annotateurId)
+          .set({ 'id': annotateurId, 'userId': annotateur.id, 'projectId': projectId });
+        console.log('Annotateur ajouté');
+      }
+
+    });
+  }
+
+  // Ajoute une nouvelle catégories
+  addCategorie(categorie: any, projectId: string) {
+    // flag si la nouvel catégorie existe déjà
+    let categoryExist = false;
+
+    this.afs.collection('Categories').ref.where('projectId', '==', projectId).get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+
+        // Vérifie si la nouvelle catégorie n'utilise pas un nom ou une couleur déjà utilisé
+        if (doc.data().color === categorie.categoryColor) {
+          categoryExist = true;
+          console.log('La couleur choisit est déjà utilisé par une autre catégorie');
+
+        } else if ((doc.data().name as string).toUpperCase() === (categorie.categoryName as string).toUpperCase()) {
+          categoryExist = true;
+          console.log('Le nom choisit est déjà utilisé par une autre catégorie');
+        }
+
+      });
+
+      if (categoryExist === false) {
+
+        const categorieId = this.afs.createId();
+        // ajoute la nouvelle catégorie
+        this.afs.collection('Categories').doc(categorieId)
+          .set({
+            'id': categorieId, 'projectId': projectId,
+            'color': categorie.categoryColor, 'name': categorie.categoryName
+          });
+        console.log('Catégorie Ajouté');
+      }
+    });
   }
 
 }

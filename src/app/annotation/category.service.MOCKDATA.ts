b69bf9e6376a1dc61388ef818407bf730d22b3a1
mago01 <mago01@uqo.ca>
2018-09-27 10:46:30 -0400

    Initialisation des Tests de catégory service avec Mock Data

---
+++ src/app/annotation/category.service.MOCKDATA.ts
@@ -0,0 +1,8 @@
+// Format de données MOCK pour les catégories
+
+export const MOCK_CATEGORIES = [
+  { color: '#4CAF50', name: 'Regulier' },
+  { id: '#FFEB3B', nom: 'Nom avec espaces'},
+  { id: '#673AB7', nom: 'Nom avec Charactères "&!(/! spéciaux'},
+  { id: '#673AB7', nom: 'Nom avec charactères escaped \''},
+];

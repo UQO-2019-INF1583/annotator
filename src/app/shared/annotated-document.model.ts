ea58c1a1c9a378961d6a816ba9c3ff5e5251fd22
Bruno Hamel <bruno.hamel93@gmail.com>
2018-11-10 12:03:47 -0500

    Start of the json deserializer for the annotated document

--- src/app/shared/annotated-document.model.ts
+++ src/app/shared/annotated-document.model.ts
@@ -10,6 +10,7 @@ import {
   rawAttribute,
   rawEntity
 } from '../annotation/document-data';
+import { Project } from './project.model';
 
 type rangeTextSelection = [number, number];
 type id = string;
@@ -60,7 +61,63 @@ export class AnnotatedDocument extends Doc {
     this.text = text;
   }
 
-  fromJSON(json: string): void {}
+  fromJSON(json: string, project: Project): void {
+    const docData: DocumentData = JSON.parse(json);
+
+    this.entities = docData.entities.map(docEntity => {
+      const entity: EntityAnnotation = null;
+      const project_entity = project.entities.find(e => e.type === docEntity[1])
+
+      // Document entity
+      entity.id = docEntity[0];
+      entity.locations = docEntity[2];
+
+      // Project entity
+      entity.name = project_entity.name;
+      entity.type = project_entity.type;
+      entity.bgColor = project_entity.bgColor;
+      entity.labels = project_entity.labels;
+
+      return entity;
+    });
+
+    this.attributes = docData.attributes.map(docAttribute => {
+      const attribute: AttributeAnnotation = null;
+      const project_attribute = project.attributes.find(a => a.type === docAttribute[1])
+
+      // Document attribute
+      attribute.id = docAttribute[0];
+      attribute.type = docAttribute[1];
+      attribute.target = docAttribute[2];
+
+      // Project attribute
+      attribute.name = project_attribute.name;
+      attribute.valeurs = project_attribute.valeurs;
+
+      return attribute;
+    });
+
+    this.relations = docData.relations.map(docRelation => {
+      const relation: RelationAnnotation = null;
+      const project_relation = project.relations.find(r => r.type === docRelation[1])
+
+      // Document relation
+      relation.id = docRelation[0];
+      relation.type = docRelation[1];
+      relation.from.id = docRelation[2][0][0];
+      relation.from.role = docRelation[2][0][1];
+      relation.to.id = docRelation[2][1][0];
+      relation.to.role = docRelation[2][1][1];
+
+      // Project relation
+      relation.color = project_relation.color;
+      relation.entity = project_relation.entity;
+      relation.name = project_relation.name;
+
+      return relation;
+    });
+
+  }
 
   toJSON(): string {
     const docData: DocumentData = null;

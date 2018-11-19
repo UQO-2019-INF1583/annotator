729d5bee090995ddfc770b8da372d9feaf58f28f
JMCharbonneau <chaj159@uqo.ca>
2018-11-19 16:05:31 -0500

    Version final des tests de brat-utils

--- src/app/annotation/brat/brat-utils.mock.ts
+++ src/app/annotation/brat/brat-utils.mock.ts
@@ -4,49 +4,35 @@ import { AnnotatedDocument } from '../../shared/annotated-document.model';
 import { CollectionData } from '../collection-data';
 
 export const docData = {
-  valid1 : {
-    messages: [],
-    source_files: ['ann', 'text'],
-    modifications: [],
-    normalizations: [],
-    ctime: 1351154734.5055847,
+  expectedDocData : {
     text: 'Ed O\'Kelley was the man who shot the man who shot Jesse James.\nJ\'ai castor le plus petit, mais le plus fort.',
     entities: [
-      ['N1', 'Person', [[0, 2], [5, 11]]],
-      ['N2', 'Person', [[20, 55], [55, 90], [90, 124]]],
-      ['N3', 'Person', [[37, 40]]],
-      ['N4', 'Object', [[78, 83], [84, 93]]],
-      ['N5', 'Person', [[98, 104]]],
-      ['N6', 'Person', [[105, 111]]],
-      ['N7', 'Person', [[115, 120]]],
-      ['N8', 'Person', [[50, 61]]]
+      ['N1', 'Person', [[0, 2]]],
+      ['N2', 'Person', [[5, 11]]]
     ],
     attributes: [
-      ['A1', 'Notorious', 'N4'],
-      ['A2', 'Polarity', 'N1', 'Positive'],
-      ['A3', 'Polarity', 'N2', 'Negative'],
-      ['A4', 'Epic', 'T1'],
-      ['A5', 'Safe', 'R1']
+      ['A1', 'Notorious', 'N1']
     ],
     relations: [
-      ['R1', 'Friend', [['From', 'N2'], ['To', 'N1']]]
+      ['R1', 'Friend', [['From', 'N1'], ['To', 'N2']]]
     ],
     triggers: [
-      ['T1', 'Assassination', [[45, 49]]],
-      ['T2', 'Resurrection', [[28, 32]]],
-      ['T3', 'Bomb', [[78, 93]]]
+      ['T1', 'Friend', [[3, 4]] ]
     ],
     events: [
-      ['E1', 'T1', [['Perpetrator', 'N3'], ['Victim', 'N8']]],
-      ['E2', 'T2', [['Savior', 'N2'], ['Resurrected', 'N3']]],
-      ['E3', 'T3', [['Destroyed', 'N5'], ['Destroyed', 'N6'], ['Destroyed', 'N7']]]
+      ['E1', 'T1', [['Greeter', 'N1'], ['Greeted', 'N2']]]
     ],
     comments: [
       ['N1', 'AnnotatorNotes', 'test comment']
-    ]
-  } as DocumentData,
-  docRes1 : {
-    text: 'Ed O\'Kelley was the man who shot the man who shot Jesse James.\nJ\'ai castor le plus petit, mais le plus fort.',
+    ],
+    ctime: 1351154734.5055847,
+    messages: [],
+    modifications: [],
+    normalizations: [],
+    source_files: []
+  }as DocumentData,
+  stdDocData : {
+    text: 'Ed O\'Kelley was ,the man who shot the man who shot Jesse James.\nJ\'ai castor le plus petit, mais le plus fort.',
     entities: [
       ['N1', 'Person', [[0, 2]]],
       ['N2', 'Person', [[5, 11]]]
@@ -74,7 +60,7 @@ export const annotDoc = {
     title: 'test1',
     file: 'test1',
     text: 'Ed O\'Kelley was the man who shot the man who shot Jesse James.\nJ\'ai castor le plus petit, mais le plus fort.',
-    projectId: 'testP1',
+    projectId: 'test1',
     entities: [
       {
         id: 'N1',
@@ -87,7 +73,7 @@ export const annotDoc = {
         ],
         type: '',
         labels: [],
-        bgColor: '',
+        bgColor: '#FE2E2E',
         borderColor: '',
         unused: false,
         arcs: [],
@@ -104,7 +90,7 @@ export const annotDoc = {
         ],
         type: '',
         labels: [],
-        bgColor: '',
+        bgColor: '#FE2E2E',
         borderColor: '',
         unused: false,
         arcs: [],
@@ -147,13 +133,16 @@ export const annotDoc = {
         locations: [
           {
             start: 3,
-            end: 7
+            end: 4
           }
         ],
-        links: [],
+        links: [{
+          id: 'N1',
+          type: 'Greeter'
+        }],
         triggerId: 'T1',
-        name: 'TestE',
-        type: 'Test',
+        name: 'Greeting',
+        type: 'Friend',
         labels: [],
         bgColor: '',
         borderColor: 'darken',
@@ -164,12 +153,12 @@ export const annotDoc = {
       }
     ]
   }as AnnotatedDocument,
-  doc2: {
-    documentId: 'test2',
-    title: 'test2',
-    file: 'test2',
-    text: 'test1',
-    projectId: 'testP2',
+  aDocInit: {
+    documentId: 'test1',
+    title: 'test1',
+    file: 'test1',
+    text: 'Ed O\'Kelley was the man who shot the man who shot Jesse James.\nJ\'ai castor le plus petit, mais le plus fort.',
+    projectId: 'test1',
     entities: [],
     attributes: [],
     relations: [],
@@ -179,15 +168,15 @@ export const annotDoc = {
 
 export const project = {
   proj1: {
-    id: 'test3',
-    title: 'test3',
-    description: 'test3',
+    id: 'test1',
+    title: 'test1',
+    description: 'test1',
     admin: [],
     annotators: [],
     corpus: [],
     entities: [
       {
-        name: 'person',
+        name: 'Person',
         type: '',
         labels: [],
         bgColor: '#FE2E2E',
@@ -208,8 +197,8 @@ export const project = {
     ],
     events: [
       {
-        name: 'testE',
-        type: '',
+        name: 'Greeting',
+        type: 'Friend',
         labels: [],
         bgColor: '',
         borderColor: 'darken',
@@ -233,7 +222,7 @@ export const project = {
 }
 
 export const colData = {
-  colRes1 : {
+  stdColData : {
     items: [],
     messages: [],
     search_config: [
@@ -254,7 +243,7 @@ export const colData = {
     },
     entity_types: [
       {
-        name: 'person',
+        name: 'Person',
         type: '',
         labels: [],
         bgColor: '#FE2E2E',
@@ -267,8 +256,8 @@ export const colData = {
     ],
     event_types: [
       {
-        name: 'testE',
-        type: '',
+        name: 'Greeting',
+        type: 'Friend',
         labels: [],
         bgColor: '',
         borderColor: 'darken',
--- src/app/shared/annotated-document.model.ts
+++ src/app/shared/annotated-document.model.ts
@@ -110,7 +110,7 @@ export class RelationAnnotationUtils {
       dashArray: '3,3',
       color: '',
       attributes: [],
-      arcs: []
+      args: []
     }
   }
 }

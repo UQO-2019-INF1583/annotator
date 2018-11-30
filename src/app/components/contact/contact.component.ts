90ee224f05550af8c28e59d738a4ea4409824404
Siham <s.elomari27@gmail.com>
2018-11-30 09:31:47 -0500

    Amelioration Bug#47
    Ajouter une collection dans la base pour stocker les message

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AngularFirestore } from 'angularfire2/firestore';
+import * as firebase from 'firebase';
 
 @Component({
     selector: 'app-contact',
@@ -9,9 +11,19 @@ import { Router } from '@angular/router';
 })
 
 export class ContactComponent implements OnInit {
-    constructor(public router: Router) { }
+    private message: string;
+
+    constructor(private afs: AngularFirestore) {
+
+    }
 
     ngOnInit() {
     }
+    createMessage() {
+
+        this.afs.collection('messageContact').doc(this.afs.createId()).set(this.message);
+    }
+
 
 }
+

b3f33762bb22e3e1f37d7bdbd853573f37723d86
Unknown <JeanMarc.Hebert1996@gmail.com>
2018-12-04 11:35:10 -0500

    Fix bug #93
    Ajout de validation d'adresse courriel

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -3,6 +3,28 @@ import { AuthService } from '../shared/security/auth.service';
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
+  //styleUrls: ['./reset-password.component.css']
+})
+export class ResetPasswordComponent implements OnInit {
+  userInfo: any = {};
+  constructor(public auth: AuthService,) { }
+  errorMessage='';
+
+  ngOnInit() {
+
+  }
+  resetPassword(email: string) {
+   this.auth.resetPassword(this.userInfo.email).then().catch((error)=>{console.log(error);this.errorMessage=error.message})
+  }
+  emailInvalid(){
+	return this.errorMessage !== '';
+  }
+}
+/*import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/security/auth.service';
+@Component({
+  selector: 'app-reset-password',
+  templateUrl: './reset-password.component.html',
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent implements OnInit {
@@ -15,4 +37,4 @@ export class ResetPasswordComponent implements OnInit {
   resetPassword(email: string) {
    this.auth.resetPassword(this.userInfo.email);
   }
-}
+}*/

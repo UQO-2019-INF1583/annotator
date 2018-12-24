b88ce2cb428c121884df5a17178ea906d1fd1fc5
iglewski <iglewski@uqo.ca>
2018-12-23 20:53:47 -0500

    Correction (?) of bug #46 (#52) by Bubblesphere.

--- src/app/components/create-project/create-project.service.ts
+++ src/app/components/create-project/create-project.service.ts
@@ -6,13 +6,26 @@ import { Project } from '../../shared/project.model';
 @Injectable()
 export class CreateProjectService {
 
-    constructor(private afs: AngularFirestore) { }
+  constructor(private afs: AngularFirestore) { }
 
-    createNewProject(project: Project) {
-        project.id = this.afs.createId();
-        const currentUserId = firebase.auth().currentUser.uid;
-        project.admin.push(currentUserId);
-        project.annotators.push(currentUserId);
-        this.afs.collection('Projects').doc(project.id).set(project);
-    }
+  createNewProject(project: Project) {
+    project.id = this.afs.createId();
+    const currentUserId = firebase.auth().currentUser.uid;
+    project.admin.push(currentUserId);
+    project.annotators.push(currentUserId);
+    this.afs.collection('Projects').doc(project.id).set(project);
+  }
+
+  projectNameExists(project: Project): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this.afs
+        .collection<Project>('Projects')
+        .ref
+        .where('title', '==', project.title)
+        .get()
+        .then(projects => (
+          resolve(projects.size >= 1)
+        ));
+    });
+  }
 }

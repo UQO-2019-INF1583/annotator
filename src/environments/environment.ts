91675e00357880e61691055274ab3a7dc712e8aa
TheCoolestFridge <jonathanfrigault1@gmail.com>
2019-09-17 15:35:12 -0400

    added environment file for our team
    the environment file is for our database

---
+++ src/environments/environment.ts
@@ -0,0 +1,22 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+/*
+----------------
+Group 1 database
+----------------
+*/
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCE3ValPRyX8hGaQ9wT4h1o-Kw2YasHCOM",
+    authDomain: "test-ef3fe.firebaseapp.com",
+    databaseURL: "https://test-ef3fe.firebaseio.com",
+    projectId: "test-ef3fe",
+    storageBucket: "test-ef3fe.appspot.com",
+    messagingSenderId: "498691035407"
+  }
+};
+

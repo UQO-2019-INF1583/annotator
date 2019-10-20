7d4a9b4c11189532320d86d5e7692198033d55af
zenAndroid <zena02@uqo.ca>
2019-10-20 16:01:51 -0400

    Ajout de commentaires pour mieux exliquer les modifications.

--- src/app/adm/userManager/userManager.component.ts
+++ src/app/adm/userManager/userManager.component.ts
@@ -23,6 +23,8 @@ import { User } from '../../shared/user.model';
 export class UserManagerComponent implements OnInit {
 
   displayedColumns = ['email', 'firstname', 'lastname', 'role', 'modify'];
+  // Sert à dire au coposant HTML le colonnes à afficher.
+  // Voir https://v7.material.angular.io/components/table/overview#3-define-the-row-templates pour plus de detail
   dataSource: UserDataSource | null;
 
   constructor(public router: Router,
@@ -42,7 +44,17 @@ export class UserManagerComponent implements OnInit {
   // Modification des informations d'un utilisateur
   modifyUser(user: any) {
     // ajouter un pop up pour la modification
-    let roleString: string;
+    let roleString: string; /* Variable qui va retenir la représentation textuelle du role
+    Puisque le rôle de l'utilisateur est représenté comme entier de tel manière que :
+     0 -> Visiteur
+     1 -> Membre
+     2 -> Administrateur
+     Alors il suffit de tester user.role et d'affecter la représentation correspondante
+     J'utilise un switch parceque c'est plus intuitif.
+
+     Le but de la création de cette variable est de pouvoir la passer au constructeur
+     de EditUserComponent pour que, quand il s'instanciera il affichera le rôle de l'utilisateur.
+     */
     switch (user.role) {
       case 0: {
         roleString = 'Visiteur';
@@ -66,7 +78,7 @@ export class UserManagerComponent implements OnInit {
         lastname: user.lastname,
         email: user.email,
         role: user.role,
-        roleRepresent: roleString,
+        roleRepresent: roleString, // <-- On la passe comme attribut de l'objet temporaire data
         roles: [
           { valeur: 1, viewValue: 'Membre' },
           { valeur: 2, viewValue: 'Administrateur' },

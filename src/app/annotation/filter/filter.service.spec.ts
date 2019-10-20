f4f2fd0768f7ccfa2710f0080a42a457baa8a492
MarshallBAGUI <54874313+MarshallBAGUI@users.noreply.github.com>
2019-10-20 14:53:32 -0400

    Update filter.service.spec.ts

--- src/app/annotation/filter/filter.service.spec.ts
+++ src/app/annotation/filter/filter.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FilterService } from './filter.service';
-
+/* cette methode vérifie que chaque module respecte les normes avant d'être testé */
 describe('FilterService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
     const service: FilterService = TestBed.get(FilterService);
-    expect(service).toBeTruthy();
+    expect(service).toBeTruthy(); / on s'assure que le resultat obtenu correspond bien au resultat de l'objet créé */
   });
 });

f7722b4c103abb74310483cb3e6e577558b3a5fe
JM <alijm0369@gmail.com>
2019-11-07 10:56:31 -0500

    Queleus modifications apporte afin de permettre la gestion des annotateurs multiples.
    Des changements sont a avenir

--- src/app/annotation/annotation.service.ts
+++ src/app/annotation/annotation.service.ts
@@ -1,44 +1,108 @@
 import { Injectable } from '@angular/core';
-import { Entity } from '../shared/entity.model';
+// import { Entity } from '../shared/entity.model';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AnnotatedDocument } from '../shared/annotated-document.model';
 import { BratUtils } from './brat/brat-utils';
+import * as firebase from 'firebase';
+import { config } from 'rxjs';
+
+
+// import { Entity } from '../shared/security/auth.service.ts'
+
 
 @Injectable()
 export class AnnotationService {
 
+  currentUserId: string;
+
+
   constructor(private readonly afs: AngularFirestore) {
+    this.currentUserId = firebase.auth().currentUser.uid;
   }
+
 
   // Retourne de façon asynschore le document de type project, dont le id est passé en paramètre, à partir de la base de données Firestore
   getProject(projectId): Promise<any> {
     return this.afs.collection('Projects/').doc(projectId).ref.get();
   }
-  getUserID(uid): Promise<any> {
-    return this.afs.collection('Users/').doc(uid).ref.get();
-  }
+
+
 
   saveAnnotatedDocument(annotatedDocument: AnnotatedDocument): void {
-    if (annotatedDocument.documentId === null) {
-      annotatedDocument.documentId = this.afs.createId();
-      //assigner le userID au document lorsque l'annotateur y accède pour la 1re fois
-      // annotatedDocument.userId = firebase.auth().currentUser.uid;
 
-    }
+    // On initie le chmapps le userID s'il n'ete pas deja fait
+    const currentUserId = firebase.auth().currentUser.uid;
+    annotatedDocument.userId = currentUserId;
 
-    this.afs
-      .collection('AnnotatedDocument')
-      .doc(annotatedDocument.documentId)
-      .set(Object.assign({}, annotatedDocument));
+    /* If the document does not exist, it will be created. If the document does exist, its
+      contents will be overwritten with the newly provided data */
+    const citiesRef = this.afs.collection('AnnotatedDocument');
+    citiesRef.doc(annotatedDocument.documentId + currentUserId).set(annotatedDocument);
+
   }
+
 
   getAnnotatedDocument(documentId: string): Promise<any> {
-    return this.afs.collection('AnnotatedDocument/').doc(documentId).ref.get();
+    // On lui envoie la copie qui lui est associe.  Cette copie a ete cree lorsqu'il a clique sur view
+    return this.afs.collection('AnnotatedDocument/').doc(documentId + this.currentUserId).ref.get();
   }
 
-  //fonction qui devrait aller chercher le document associé a l'annotateur
-  getAnnotatorDocument(documentId: string): Promise<any> {
-    return this.afs.collection('AnnotatatedDocument/').doc(documentId).ref.get();
+
+
+
+
+
+  // Fonction qui va retourner le nom de tous les Annotated Document corespondant a un Corpus specific
+  getAllAnnotatedDocuments(corpusId: string): string[] {
+
+
+    // Variable qui va nous permettre d'utiliser les codes ci-dessous afin d'acceder au BD et
+    // itere a travers les documents (AnnotatedDocuments)
+    const db = firebase.firestore();
+
+    // Variable qui va contenir le nom de tous les AnnotatedDocuments qui se trouvent dans mon corpus
+    var AllAnnotatedDocumentsIds = new Array();
+
+
+    db.collection('AnnotatedDocument/').get().then((snapshot) => {
+      snapshot.docs.forEach(doc => {
+
+        if (corpusId === doc.data().documentId) {
+          AllAnnotatedDocumentsIds.push(doc.data().userId);
+          console.log('userIds: ' + doc.data().userId);
+        }
+      })
+    })
+
+    var clothing = ['shoes', 'shirts', 'socks', 'sweaters'];
+    console.log(clothing.length);
+
+    console.log('La taille de mon tableau=  ' + AllAnnotatedDocumentsIds);
+    return AllAnnotatedDocumentsIds;
   }
 
 }
+
+
+    // On a accee aux information general des element de la collections
+/*
+    db.collection('AnnotatedDocument/').get().then((snapshot) => {
+      console.log(snapshot.docs);
+    })
+*/
+
+
+/*
+// Accee au document complet
+console.log('Doc Data:  ');
+console.log(doc.data());
+
+// Accee au document id (nom du document dans la BD)
+console.log('Doc.id: ');
+console.log(doc.id);
+
+// Accee a un champ precis document Id
+console.log('documentId: ');
+console.log(doc.data().documentId);
+
+*/

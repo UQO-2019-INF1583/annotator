a327a7dced3b97ec4ee9ea70b5762ba303887336
EstherN <esthernolaki@gmail.com>
2019-11-16 21:17:38 -0500

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -21,6 +21,10 @@ export class LoginComponent implements OnInit {
     localStorage.removeItem("errorAuth");
   }
 
+needsLogin() {
+    return !this.authService.isAuthenticated();
+  }
+  
   private handleError(error: Error): void {
     this.loading = false;
     this.errorMessage = error.message;

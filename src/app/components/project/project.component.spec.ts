fd4560d2e5a91e20ad00f0bd67eadc2468fda454
TheCoolestFridge <jonathanfrigault1@gmail.com>
2019-11-18 11:21:00 -0500

    Remove not working test in project

--- src/app/components/project/project.component.spec.ts
+++ src/app/components/project/project.component.spec.ts
@@ -30,7 +30,6 @@ import { YesNoDialogBoxComponent } from '../yes-no-dialog-box/yes-no-dialog-box.
 import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 import { Observable } from 'rxjs/Observable';
 
-/* trop d'erreurs*/
 describe('Projet', () => {
   let projectComponent: ProjectComponent;
   let projectFixture: ComponentFixture<ProjectComponent>;
@@ -76,7 +75,7 @@ describe('Projet', () => {
   it('should create component', () => {
     expect(projectComponent).toBeDefined();
   });
-
+  //test related to entities
   describe('Entities', () => {
     it('should add the entite to the current project\'s entities if provided { with a valid } result', () => {
       projectComponent.addEntitiesAfterClosedHandler(entiteMock.valid3);
@@ -124,7 +123,7 @@ describe('Projet', () => {
       );
     });
   });
-
+  //test related to attributs
   describe('Attributs', () => {
     it('should add the attribute to the current project\'s attributes if provided { with a valid } result', () => {
       projectComponent.addAttributesAfterClosedHandler(attributMock.valid1);
@@ -153,79 +152,79 @@ describe('Projet', () => {
       );
     });
   });
-  /*
-    describe('Relations', () => {
-      it('should alert the user when trying to add a relation that already exists', () => {
-        spyOn(window, 'alert');
-        projectComponent.addRelation(relationMock.valid1);
-        projectComponent.addRelation(relationMock.valid1);
-        expect(window.alert).toHaveBeenCalledWith('This relation already exists');
-      });
-  
-      it('should add the relation to the current project\'s relation', () => {
-        projectComponent.addRelation(relationMock.valid1);
-        expect(projectComponent.currentProject.relations).toContain(relationMock.valid1);
-      });
-  
-      it('should be able to delete a relation', () => {
-        // Adding valid1
-        projectComponent.addRelation(relationMock.valid1);
-        expect(projectComponent.currentProject.relations).toContain(
-          relationMock.valid1
-        );
-  
-        // Removing valid1
-        projectComponent.deleteRelation(relationMock.valid1);
-        expect(projectComponent.currentProject.relations).not.toContain(
-          relationMock.valid1
-        );
-      });
-  
-      it('should not add an undefined relation to the current project\'s relation', () => {
-        projectComponent.addRelation(relationMock.valid1);
-        expect(projectComponent.currentProject.relations.length).toBe(1);
-        projectComponent.addRelation(relationMock.undefined);
-        expect(projectComponent.currentProject.relations.length).toBe(1);
+  //test related to relation
+  describe('Relations', () => {
+    it('should alert the user when trying to add a relation that already exists', () => {
+      spyOn(window, 'alert');
+      projectComponent.addRelation(relationMock.valid1);
+      projectComponent.addRelation(relationMock.valid1);
+      expect(window.alert).toHaveBeenCalledWith('This relation already exists');
+    });
+
+    it('should add the relation to the current project\'s relation', () => {
+      projectComponent.addRelation(relationMock.valid1);
+      expect(projectComponent.currentProject.relations).toContain(relationMock.valid1);
+    });
+
+    it('should be able to delete a relation', () => {
+      // Adding valid1
+      projectComponent.addRelation(relationMock.valid1);
+      expect(projectComponent.currentProject.relations).toContain(
+        relationMock.valid1
+      );
+
+      // Removing valid1
+      projectComponent.deleteRelation(relationMock.valid1);
+      expect(projectComponent.currentProject.relations).not.toContain(
+        relationMock.valid1
+      );
+    });
+
+    it('should not add an undefined relation to the current project\'s relation', () => {
+      projectComponent.addRelation(relationMock.valid1);
+      expect(projectComponent.currentProject.relations.length).toBe(1);
+      projectComponent.addRelation(relationMock.undefined);
+      expect(projectComponent.currentProject.relations.length).toBe(1);
+    });
+  });
+  //test related to events
+  describe('Events', () => {
+    /*it('should throw an error if the closing payload is null', () => {
+      const closedHandlerPayload = null;
+      expect(() => {
+        projectComponent.addEventAfterClosedHandler(closedHandlerPayload);
+      }).toThrow();
+    });*/
+
+    it('should map result to event properly', () => {
+      expect(projectComponent.mapValidResultToEvent(eventMock.valid1)).toEqual({
+        name: eventMock.valid1.name,
+        attributes: eventMock.valid1.attributes[0].split(','),
+        labels: eventMock.valid1.labels[0].split(','),
+        type: eventMock.valid1.type,
+        bgColor: eventMock.valid1.bgColor,
+        arcs: [],
+        borderColor: 'darken',
+        children: [],
+        unused: false
       });
     });
-    /*
-      describe('Events', () => {
-        it('should throw an error if the closing payload is null', () => {
-          const closedHandlerPayload = null;
-          expect(() => {
-            projectComponent.addEventAfterClosedHandler(closedHandlerPayload);
-          }).toThrow();
-        });
-    
-        it('should map result to event properly', () => {
-          expect(projectComponent.mapValidResultToEvent(eventMock.valid1)).toEqual({
-            name: eventMock.valid1.name,
-            attributes: eventMock.valid1.attributes[0].split(','),
-            labels: eventMock.valid1.labels[0].split(','),
-            type: eventMock.valid1.type,
-            bgColor: eventMock.valid1.bgColor,
-            arcs: [],
-            borderColor: 'darken',
-            children: [],
-            unused: false
-          });
-        });
-    
-        it('should add the event to the current project\'s events if provided with a valid result', () => {
-          projectComponent.addEventAfterClosedHandler(eventMock.valid1);
-          expect(projectComponent.currentProject.events).toContain(
-            projectComponent.mapValidResultToEvent(eventMock.valid1)
-          );
-        });
-    
-        it('should alert the user when trying to add an event that already exists', () => {
-          spyOn(window, 'alert');
-          projectComponent.addEventAfterClosedHandler(eventMock.valid1);
-          projectComponent.addEventAfterClosedHandler(eventMock.valid1);
-          expect(window.alert).toHaveBeenCalledWith('This event already exists');
-        });
-      });
-    */
+
+    it('should add the event to the current project\'s events if provided with a valid result', () => {
+      projectComponent.addEventAfterClosedHandler(eventMock.valid1);
+      expect(projectComponent.currentProject.events).toContain(
+        projectComponent.mapValidResultToEvent(eventMock.valid1)
+      );
+    });
+
+    it('should alert the user when trying to add an event that already exists', () => {
+      spyOn(window, 'alert');
+      projectComponent.addEventAfterClosedHandler(eventMock.valid1);
+      projectComponent.addEventAfterClosedHandler(eventMock.valid1);
+      expect(window.alert).toHaveBeenCalledWith('This event already exists');
+    });
+  });
+  //test related to annotators
   describe('Annotators', () => {
     it('should be able to add an annotator', () => {
       projectComponent.addAnnotatorAfterClosedHandler(annotatorMock.result.valid1);

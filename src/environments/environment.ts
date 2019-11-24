115c92130ce1e639dcb00680fbbcc95a5e955954
VictorBlaja <victor.blaja@hotmail.com>
2019-11-24 00:49:32 -0500

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyBFckAdAHbV6BCDG_JQl5mO8ZVDRH1UUws',
+        authDomain: 'first-d6952.firebaseapp.com',
+        databaseURL: 'https://first-d6952.firebaseio.com',
+        projectId: 'first-d6952',
+        storageBucket: 'first-d6952.appspot.com',
+        messagingSenderId: '562292151585'
+    }
+};

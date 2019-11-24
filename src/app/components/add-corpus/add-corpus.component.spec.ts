cfef9c84fbe7631d4cbdcc6322d53e0730c124f0
Marie-Helene Tanguay-Berube <mh.tanguayberube@gmail.com>
2019-11-24 15:12:00 -0500

    Correction du bug 132

---
+++ src/app/components/add-corpus/add-corpus.component.spec.ts
@@ -0,0 +1,79 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AddCorpusComponent } from "./add-corpus.component";
+import { FormsModule } from "@angular/forms";
+import {
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatInputModule,
+  MatDialogModule,
+  MatProgressBarModule
+} from "@angular/material";
+import {
+  NgxFileDropModule,
+  NgxFileDropEntry,
+  FileSystemFileEntry,
+  FileSystemDirectoryEntry
+} from "ngx-file-drop";
+
+fdescribe("AddCorpusComponent", () => {
+  let component: AddCorpusComponent;
+  let fixture: ComponentFixture<AddCorpusComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        NgxFileDropModule,
+        MatInputModule,
+        MatDialogModule,
+        MatProgressBarModule,
+        BrowserAnimationsModule
+      ],
+      declarations: [AddCorpusComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: { corpusTitle: undefined, corpusFile: undefined }
+        }
+      ]
+    }).compileComponents();
+    fixture = TestBed.createComponent(AddCorpusComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("drop file", () => {
+    component.fileDropped(mockFile);
+    expect(component.data.corpusFile).toEqual(jasmine.objectContaining(file));
+  });
+});
+
+// Mock data
+const mockFile: NgxFileDropEntry[] = [
+  {
+    relativePath: "endo-1.txt",
+    fileEntry: {
+      isDirectory: false,
+      isFile: true,
+      name: "endo-1",
+      file: (callback: (files: File) => void) => {
+        callback(file);
+        return file;
+      }
+    } as FileSystemFileEntry
+  }
+];
+
+const file: File = {
+  lastModified: 1572726788132,
+  name: "travail2.txt",
+  size: 158,
+  type: "text/plain",
+  slice: null
+};

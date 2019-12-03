04f21f73f4d0de65aa49c5a97ceebf0e0e71f6e3
EstherN <esthernolaki@gmail.com>
2019-12-02 23:16:03 -0500

    Update event.model.ts
    Ajout d'un constructeur pour les event_type

--- src/app/shared/event.model.ts
+++ src/app/shared/event.model.ts
@@ -5,19 +5,20 @@ export class Event {
   bgColor: string;
   borderColor: string;
   attributes: string[];
-  children: any[];
+  children: Event[];
   unused: boolean;
-  arcs: any[];
+  arcs: Arc[];
 
-  constructor() {
-    this.name = '';
-    this.type = '';
-    this.labels = [];
-    this.bgColor = '';
+  constructor(name: string = '', type: string = '', labels: string[] = [], bgColor: string = ''
+    , borderColor: string = 'darken', attributes: string[] = [], unused: boolean = false, arcs: Arc[] = [], children: Event[] = []) {
+   this.name = name;
+    this.type = type;
+    this.labels = labels;
+    this.bgColor = bgColor;
     this.borderColor = 'darken';
-    this.attributes = [];
-    this.children = [];
-    this.unused = false;
-    this.arcs = [];
+    this.attributes = attributes;
+    this.children = children;
+    this.unused = unused;
+    this.arcs = arcs;
   }
 }

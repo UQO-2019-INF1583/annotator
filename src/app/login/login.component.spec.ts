8743a4ea7302ca9c70ef077efbdaae2d1a810f52
lalou <lamourlouis@hotmail.com>
2019-12-02 20:06:51 -0500

    implementation du 2eme fonction de test

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -9,8 +9,14 @@ class MockAuthService {
   }
 }
 
-fdescribe('Component: Login', () => {
+class userInfo {
+  email:string;
+  password:string;
+  //static password: ;
+}
 
+fdescribe('Component: Login', () => {
+  let user: userInfo;
   let component: LoginComponent;
   let service: MockAuthService;
 
@@ -38,4 +44,11 @@ fdescribe('Component: Login', () => {
     expect(component.errorMessage='Email Required').toBe("Email Required");
   });//fin 2eme test
 
+  //3eme TEst
+ xit('Verification De la fonction login() avec email', () => {
+    service.authenticated = true;
+    // @ts-ignore
+    expect(login.signIn("abc@abc.com")).toBe('abc@abc.com');
+   // expect(component.login().signIn( userInfo.password).toBe('abc123'));
+  });//fin 3eme test
 });
